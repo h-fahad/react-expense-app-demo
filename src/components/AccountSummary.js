792f49a24db58dfc3a2445240661c63c3b07133c
h-fahad <64426351+h-fahad@users.noreply.github.com>
2021-03-14 22:40:11 -0700

    Commit Ui, Context and Reducer

---
+++ src/components/AccountSummary.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import "./../App.css"
+
+export const AccountSummary = () => {
+    return (
+        <div className="inc-exp-container">
+            <div>
+                <h4>
+                    Income
+                </h4>
+                <p>
+                    $0.00
+                </p>
+            </div>
+            <div>
+                <h4>
+                    Expense
+                </h4>
+                <p>
+                    $0.00
+                </p>
+            </div>
+
+        </div>
+    )
+}
+
---
+++ src/components/TransactionHistory.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export const TransactionHistory = () => {
+    return (
+        <div>
+            <h3>
+                TransactionHistory
+            </h3>
+            <ul className='list'>
+                <li className='plus'>
+                    Project 1 Income
+                    <span>$1,000</span>
+                    <button className='delete-btn'>X</button>
+                </li>
+                <li className='minus'>
+                Project 1 Salaries
+                    <span>-$500</span>
+                    <button className='delete-btn'>X</button>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
